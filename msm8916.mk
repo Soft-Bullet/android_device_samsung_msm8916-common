a90fbf26eddf962651457cad6f56dc77a3e6bd3b
Jens Lody <jens@jenslody.de>
2019-01-04 00:34:33 +0100

    msm8916-common: Build FMRadio from Qcom

--- msm8916.mk
+++ msm8916.mk
@@ -199,10 +199,7 @@ TARGET_EXCLUDES_AUDIOFX := true
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+    FMRadio
 
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
