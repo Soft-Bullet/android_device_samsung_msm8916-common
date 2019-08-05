1fed21656c290a6f6aae8f3c0fc8dd0865d7e482
vlw <vlwwwwww@gmail.com>
2019-08-05 16:03:31 +0300

    msm8916-common: Binderize GPS
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -104,7 +104,8 @@ PRODUCT_PACKAGES += \
 
 # GPS
 PRODUCT_PACKAGES += \
-    android.hardware.gnss@1.0-impl
+    android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service
 
 PRODUCT_PACKAGES += \
     gps.msm8916
