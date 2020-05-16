350de436c9726a0b3c97a09960057978124d97d2
vlw <vlwwwwww@gmail.com>
2020-05-16 21:10:24 +0300

    msm8916-common: Remove librecovery_updater
    * Doesn't need for our devices
    
    Signed-off-by: vlw <vlwwwwww@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -266,10 +266,6 @@ PRODUCT_PACKAGES += \
     libkeyutils \
     tcpdump
 
-# Recovery
-PRODUCT_PACKAGES += \
-    librecovery_updater_cm
-
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl \
