72bfcfb9ead22b3de95f04d75c257c37a92bfe14
Davide Garberi <dade.garberi@gmail.com>
2020-05-17 22:59:32 +0200

    msm8916-common: binderize power HAL

--- msm8916.mk
+++ msm8916.mk
@@ -385,9 +385,7 @@ PRODUCT_COPY_FILES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service \
-    power.qcom
+    android.hardware.power@1.2-service-qti
 
 # Radio
 PRODUCT_PACKAGES += \
